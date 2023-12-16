e2f00ffff9e3b8064b707c3d34106e2a6f1b9d04
crisbennie19 <akowechristian@gmail.com>
2023-12-16 18:46:15 +0100

    Single container

--- Shopping/Shopping.Client/obj/Release/net8.0/Shopping.Client.AssemblyInfo.cs
+++ Shopping/Shopping.Client/obj/Release/net8.0/Shopping.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shopping.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48e64887881b9c554892fc062dffee0814b8222d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shopping.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shopping.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
